18e41741c74a1c987f5b9c5f406f8768dec14259
unknown <gokmenoz89@gmail.com>
2019-03-07 15:14:00 -0500

    'k'

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask,render_template,request
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def index_lulu():
+    return render_template('ZUMZ.html')
+    
+@app.route('/static/<path:path>')
+def static_file(path):
+    return app.send_static_file(os.path.join('static', path))
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+@app.route('/ZQK.html')
+def hello1():
+    return render_template('ZQK.html')
 
-if __name__ == '__main__':
-  app.run(port=33507)
+@app.route('/ZTS.html')
+def hello2():
+    return render_template('ZTS.html')
+
+if __name__ == "__main__":
+    app.debug=False
+    app.run(host='0.0.0.0')
