070dec902f130c586574cdd17b56f7ea1ab04635
unknown <gokmenoz89@gmail.com>
2019-03-15 18:04:58 -0400

    'h'

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import os
 
 app = Flask(__name__)
@@ -7,17 +7,23 @@ app = Flask(__name__)
 def index_lulu():
     return render_template('ZUMZ.html')
     
-@app.route('/static/<path:path>')
+@app.route('/static/<path:path>',methods=['GET','POST'])
 def static_file(path):
     return app.send_static_file(os.path.join('static', path))
 
-@app.route('/ZQK.html')
+@app.route('/ZQK.html',methods=['GET','POST'])
 def hello1():
-    return render_template('ZQK.html')
-
-@app.route('/ZTS.html')
+    if request.method=='GET':
+        return render_template('ZQK.html',ticker='ZQK')
+    else:
+        return request.form['name_lulu']+request.form['age_lulu']
+    
+@app.route('/ZTS.html',methods=['GET','POST'])
 def hello2():
-    return render_template('ZTS.html')
+    if request.method=='GET':
+        return render_template('ZTS.html')
+    else:
+        return render_template('ZTS.html',name=request.form['name_lulu'],age=request.form['age_lulu'])
 
 if __name__ == "__main__":
     app.run(debug=False)
